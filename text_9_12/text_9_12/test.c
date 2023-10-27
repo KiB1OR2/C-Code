a285d4c89424b3518738305ccc718c906f36f0dc
KiB1OR2 <a782825039@gmail.com>
2023-10-28 00:40:39 +0800

    后续补充和修改

--- text_9_12/text_9_12/test.c
+++ text_9_12/text_9_12/test.c
@@ -3,6 +3,15 @@
 int main()
 {
 	printf("你好，807\n");
+	printf(" % d\n", 100);//打印一个整数--%d
+	//sizeof-关键字-操作符-计算类型或者所占空间的大小（单位是字节）
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
 	return 0;
 }
 //ctrl+f5程序执行 菜单栏-调试-开始执行的不调试
